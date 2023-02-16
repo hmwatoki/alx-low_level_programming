d65515887cb9f118f423d2deba9cedebceff48aa
hmwatoki <harrisonmwatoki@gmail.com>
2023-02-16 16:35:37 +0300

    c program done

---
+++ 4-puts.c
@@ -0,0 +1,6 @@
+#include <stdio.h> 
+/* This program prints a statement about programming being like building a multilingual puzzle. */
+int main() { 
+    puts("Programming is like building a multilingual puzzle"); 
+    return 0; 
+}
