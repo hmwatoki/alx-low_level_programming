cd19e7cd42af1ef3d577b15f471f90e0d842c154
hmwatoki <harrisonmwatoki@gmail.com>
2023-02-16 17:21:59 +0300

    fixed format error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include <stdio.h> 
-/* This program prints a statement about programming being like building a multilingual puzzle. */
-int main() { 
-    puts("Programming is like building a multilingual puzzle"); 
-    return 0; 
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
