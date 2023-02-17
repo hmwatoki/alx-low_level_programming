92ffd5cf9425de21c4f8765a6b9a136b04d2e7de
hmwatoki <harrisonmwatoki@gmail.com>
2023-02-17 13:36:15 +0000

    task done

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,17 @@
- #include <stdio.h>
- int main(void) {
-    int i;
-     for (i = 97; i <= 122; i++)
-        putchar(i);
-    putchar('\n');
-     return 0;
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: print a-z
+ * Return: always return 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		putchar(i);
+		putchar('\n');
+	return (0);
 }
