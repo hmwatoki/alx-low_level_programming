2ea2a1108910a11eef70aa463c7cf4d79556663d
hmwatoki <harrisonmwatoki@gmail.com>
2023-02-18 02:11:15 +0000

    task done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -10,11 +9,11 @@ int main(void)
 int i, j;
 for (i = 0; i < 9; i++)
 {
-for (j = i + 1; j <= 9; j++) 
+for (j = i + 1; j <= 9; j++)
 {
 putchar('0' + i);
 putchar('0' + j);
-if (i != 8 || j != 9) 
+if (i != 8 || j != 9)
 {
 putchar(',');
 putchar(' ');
