a357bb73520fafb804609e0f30583f9dd94eb064
hmwatoki <harrisonmwatoki@gmail.com>
2023-02-23 16:49:38 +0300

    print most

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_most_numbers - 1-9 except for 2 and 4
+ *
+ * Return: 0
+ *
+ */
+void print_most_numbers(void)
+{
+for (int i = 0; i < 10; i++)
+{
+if (i == 2 || i == 4)
+{
+continue;
+}
+else
+{
+_putchar (i);
+}
+}
+_putchar ('\n');
+}
+
