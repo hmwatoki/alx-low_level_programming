63bc8b2eb2d0c209a403234a4d7b1d40e1a3c90e
hmwatoki <harrisonmwatoki@gmail.com>
2023-02-23 23:16:18 +0300

    done

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,14 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
-/*
- * File: main.h
- * Auth: hmwatoki
- * Description: Header file containing declarations for all functions
- *       used in the 0x03-debugging
- */
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
