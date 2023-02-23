b9fdb6f5c4dc6be9a9803d2dd1cf754edb6f308e
hmwatoki <harrisonmwatoki@gmail.com>
2023-02-23 12:53:23 +0300

    attempted task

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File: main.h
+ * Auth: hmwatoki
+ * Description: Header file containing declarations for all functions
+ *       used in 0x04-more_functions_nested_loops project.
+ */
+
+#include <stdio.h>
+
+int _isupper(int c);
+int _putchar(char c);
+
+#endif
