441ce8498d1a5cd24671363932e5d1efbc167fcf
hmwatoki <harrisonmwatoki@gmail.com>
2023-02-23 18:11:01 +0300

    fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,16 @@
 void more_numbers(void)
 {
 int i, a;
+
 for (i = 1; i <= 10; i++)
 {
 for (a = 0; a <= 14; a++)
 {
 if (a > 9)
 {
-_putchar(1 / 10 + '0');
+_putchar(a / 10 + '0');
 }
-_putchar(1 % 10 + '0');
+_putchar(a % 10 + '0');
 }
 _putchar ('\n');
 }
