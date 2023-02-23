744eb85acd3f9bfef3f7bc58094934963825ec83
hmwatoki <harrisonmwatoki@gmail.com>
2023-02-23 20:33:39 +0300

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: the lenth on line
+ * Return: void
+ *
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+if (n == 0)
+{
+_putchar ('\n');
+}
+_putchar ('_');
+}
+}
+
