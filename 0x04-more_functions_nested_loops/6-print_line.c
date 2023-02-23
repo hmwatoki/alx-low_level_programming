09cc95483d242cf5f8a033ba8c31338df27e2dda
hmwatoki <harrisonmwatoki@gmail.com>
2023-02-23 20:50:41 +0300

    line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,18 +7,17 @@
  */
 void print_line(int n)
 {
-int i;
-for (i = 0; i < n; i++)
+int x;
+if (n <= 0)
 {
-if (n == 0)
-{
-_putchar ('\n');
+_putchar('\n');
 }
 else
 {
-_putchar ('_');
-_putchar ('\n');
+for (x = 0; x < n; x++)
+{
+_putchar(95);
+}
+_putchar('\n');
 }
 }
-}
-
