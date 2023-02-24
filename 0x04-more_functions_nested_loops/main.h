aba34e1899882f459bc32ef7891620542f0918d9
hmwatoki <harrisonmwatoki@gmail.com>
2023-02-24 06:44:54 +0300

    quick fix

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -17,7 +17,7 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
-void print_diagonal(int n)
+void print_diagonal(int n);
 void print_square(int size);
 void fizz_buzz(int range);
 void print_triangle(int size);
