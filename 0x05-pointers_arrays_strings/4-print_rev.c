148441b1be119e1aa3276cb7f84777b5fc0048ac
hmwatoki <harrisonmwatoki@gmail.com>
2023-02-27 18:01:37 +0300

    fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,18 @@
 #include "main.h"
+#include "2-strlen.c"
 /**
  * print_rev - prints strings in reverse
  * @s: strings
 */
 void print_rev(char *s)
 {
-if (*s == '\0')
-return;
-else
+int i;
+char t;
+
+for (i = 0; i < _strlen(s) / 2; i++)
 {
-print_rev(s + 1);
-putchar(*s);
+t = s[i];
+s[i] = s[_strlen(s) - i - 1];
+s[_strlen(s) - i - 1] = t;
 }
 }
