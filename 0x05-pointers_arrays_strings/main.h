4717c2e237c25cc08ec21207b4dc60e778787cc8
hmwatoki <harrisonmwatoki@gmail.com>
2023-02-27 09:31:05 +0300

    task 0

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File: main.h
+ * Auth: hmwatoki
+ * Description: Header file containing declarations for all functions
+ *       used in 0x04-more_functions_nested_loops project.
+ */
+
+#include <stdio.h>
+
+void reset_to_98(int *n);
+
+#endif
