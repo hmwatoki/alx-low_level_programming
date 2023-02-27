1ab627efc47abd81a262caea12763d0d39d54afd
hmwatoki <harrisonmwatoki@gmail.com>
2023-02-27 18:05:46 +0300

    fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,13 @@
 */
 void print_rev(char *s)
 {
-int i;
-char t;
+	int index;
+	char temporary;
 
-for (i = 0; i < _strlen(s) / 2; i++)
-{
-t = s[i];
-s[i] = s[_strlen(s) - i - 1];
-s[_strlen(s) - i - 1] = t;
-}
+	for (index = 0; index < _strlen(s) / 2; index++)
+	{
+		temporary = s[index];
+		s[index] = s[_strlen(s) - (index + 1)];
+		s[_strlen(s) - (index + 1)] = temporary;
+	}
 }
