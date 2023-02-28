c179958df2ce2af9add5434a1edcebf7ec75e232
hmwatoki <harrisonmwatoki@gmail.com>
2023-02-28 05:16:36 +0300

    9 done

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,17 +7,6 @@
  * @src: source to copy
  * Return: destination
  */
-
-char *_strcpy(char *dest, char *src);
-
-/**
- * _strcpy - function declaration
- * Description: copies a string
- * @dest: destination to copy to
- * @src: source to copy from
- * Return: destination
- */
-
 char *_strcpy(char *dest, char *src)
 {
 	int index;
