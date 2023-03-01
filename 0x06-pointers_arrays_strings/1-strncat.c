5a81f48d8e731e28436869d6cebc186a7e7cea3b
hmwatoki <harrisonmwatoki@gmail.com>
2023-03-01 21:23:25 +0300

    fixed

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,9 +11,10 @@ char *_strncat(char *dest, char *src, int n)
 {
 int dest_len = strlen(dest);
 
-for (int i = 0; i < n && src[i] != '\0'; i++)
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
 dest[dest_len + i] = src[i];
 dest[dest_len + n] = '\0';
 return (dest);
 }
-
