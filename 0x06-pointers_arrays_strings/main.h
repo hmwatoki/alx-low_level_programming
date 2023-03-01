a0412277f5cb3a072f13753509fd13a7cef69c28
hmwatoki <harrisonmwatoki@gmail.com>
2023-03-01 13:56:42 +0300

    done task 0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File: main.h
+ * Auth: hmwatoki
+ * Description: Header file containing declarations for all functions
+ *       used in 0x05- pointers project
+ */
+
+#include <stdio.h>
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif
