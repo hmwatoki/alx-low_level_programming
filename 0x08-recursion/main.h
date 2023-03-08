bc2b882730b1806133a767d45138ef3e66700b0e
hmwatoki <harrisonmwatoki@gmail.com>
2023-03-08 14:52:49 +0300

    task 0 done

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File: main.h
+ * Auth: hmwatoki
+ * Description: Header file containing declarations for all functions
+ *       used in 0x08- recursion
+ */
+
+#include <stdio.h>
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
