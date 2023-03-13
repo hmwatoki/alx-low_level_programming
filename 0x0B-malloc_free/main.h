3d843417478a2ea9ec3c18ff20c4346d429f15d7
hmwatoki <harrisonmwatoki@gmail.com>
2023-03-14 01:29:01 +0300

    done task 0

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*
+ * File: main.h
+ * Auth: hmwatoki
+ * Description: Header file containing declarations for all functions
+ *       used in 0x0B- Malloc project
+ */
+
+#include <stdio.h>
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
+#endif
