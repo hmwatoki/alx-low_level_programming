6503a5f4c8b11f100a693b4089208be59f67e083
hmwatoki <harrisonmwatoki@gmail.com>
2023-04-15 03:07:40 +0300

    fix: Change exit codes and add buffer to copy files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,42 +13,42 @@
 int main(int argc, const char *argv[])
 {
 int file_from, file_to;
+char buffer[1024];
 ssize_t read_bytes;
 if (argc != 3)
 {
 fprintf(stderr, "Usage: %s file_from file_to\n", argv[0]);
-exit(0);
+exit(97);
 }
 file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
 {
 fprintf(stderr, "Error: Can't read from file %s: %s\n", argv[1], strerror(errno));
-exit(0);
+exit(98);
 }
 file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 if (file_to == -1)
 {
 fprintf(stderr, "Error: Can't write to file %s: %s\n", argv[2], strerror(errno));
-exit(0);
+exit(99);
 }
-char buffer[1024];
 while ((read_bytes = read(file_from, buffer, sizeof(buffer))) > 0)
 {
 if (write(file_to, buffer, read_bytes) != read_bytes)
 {
 fprintf(stderr, "Error: Can't write to file %s: %s\n", argv[2], strerror(errno));
-exit(0);
+exit(99);
 }
 }
 if (read_bytes == -1)
 {
 fprintf(stderr, "Error: Can't read from file %s: %s\n", argv[1], strerror(errno));
-exit(0);
+exit(98);
 }
 if (close(file_from) == -1 || close(file_to) == -1)
 {
 fprintf(stderr, "Error: Can't close file descriptor: %s\n", strerror(errno));
-exit(0);
+exit(100);
 }
 return (0);
 }
